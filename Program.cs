191cb2cefb79c764f9bb879ddd5f6875c2e58059
champsxs <32793705+champsxs@users.noreply.github.com>
2018-04-05 13:30:47 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Chaminda ..!");
+            Console.ReadKey();
+            int numone = Convert.ToInt32( Console.ReadLine());
+            int numtwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numone);
+            Console.ReadKey();
+        }
+    }
+}
