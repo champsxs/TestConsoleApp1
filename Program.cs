5b8c0d9ec7c26534dcea71db8c7f59de88555f5c
champsxs <32793705+champsxs@users.noreply.github.com>
2018-04-05 16:06:17 +0200

    Day end 2018-04-05
    sxs

--- Program.cs
+++ Program.cs
@@ -10,12 +10,58 @@ namespace TestConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Chaminda ..!");
-            Console.ReadKey();
-            int numone = Convert.ToInt32( Console.ReadLine());
-            int numtwo = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(numone);
-            Console.ReadKey();
+            double numone, numtwo = 0;
+            string fname = "Chaminda", lname = "Amunugama";
+            DateTime today_date = DateTime.Today;
+            DateTime yest_day = DateTime.Today.AddDays(-1);
+            DateTime tomo_day = DateTime.Today.AddDays(+1);
+
+            Console.WriteLine("Hello " + fname + " " + lname + "..!");
+            Console.WriteLine("Yesterdays date was :" + yest_day);
+            Console.WriteLine("Todays date is : " + today_date.ToString("dd/MM/yyyy"));
+            Console.WriteLine("Tomorrows date is :" + tomo_day);
+
+            Console.WriteLine("Hello I like to know you");
+            Console.WriteLine("What is your first name :");
+            string guest_fname = Console.ReadLine();
+            string guest_lname = Console.ReadLine();
+            Console.WriteLine("You are :" + guest_fname + " " + guest_lname + " Nice to meet you");
+
+            string ori_text = "The quick fox Jumped Over the DOG";
+            string swiched_text = ori_text.Replace("quick", "brown").Replace("DOG", "lazy dog");
+            Console.WriteLine("Original text :" + ori_text);
+            Console.WriteLine("Swiched text :" + swiched_text);
+
+            string ori_num_patn = "[1,2,3,4,5]";
+            ori_num_patn = ori_num_patn.Replace("[", "").Replace("]", "");
+            List<string> numbers = ori_num_patn.Split(',').ToList();
+            string[] to_compair = { "2", "3" };
+            string[] to_add = { "6", "7", "8", "9", "10" };
+
+            Console.WriteLine("Original number pattern" + ori_num_patn);
+            foreach (string compair in to_compair) {
+                if (numbers.Contains(compair)) {
+                    numbers.Remove(compair);
+                }
+            }
+            numbers.AddRange(to_add);
+            Console.Write("[");
+            foreach (var sxs in numbers) {
+                    Console.Write(sxs);
+                    Console.Write(",");
+            }
+            Console.WriteLine("]");
+
+            //Addition 
+            Console.WriteLine("Enter numbers to add followed by enter");
+            while (true)
+            {
+                if (double.TryParse(Console.ReadLine(), out numone))
+                {
+                    numtwo = numtwo + numone;
+                }
+                Console.WriteLine("Sum is " + numtwo);
+            }
         }
     }
 }
